106bb0ac0867ef8b495c04fb2da45665210886bf
Khushal Bharat Bamaniya <khushalbharat0@gmail.com>
2026-07-20 20:06:11 +0530

    DataFrame

---
+++ Numpy/Pandas_DataFrame.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+#Dict Data into DataFrame
+
+# data={"Id":[101,102,103],
+#        "Name":["abc","xyz","prq"],
+#        "Marks":[70,80,90],
+#        "Divsion":['A','B','C']}
+# df=pd.DataFrame(data)
+
+
+#List Data in DataFrame
+
+# data=[[1,2,3],
+#       [3,4,5],
+#       [6,7,8]]
+# df=pd.DataFrame(data,columns=["No1","No2","No3"])
+# print(df)
+
+data=pd.array([1,2,3])
+print(data)
