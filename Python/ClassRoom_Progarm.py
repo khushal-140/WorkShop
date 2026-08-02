6f68372f656ed761046c8e59ae0e260504bc606a
Khushal Bharat Bamaniya <khushalbharat0@gmail.com>
2026-08-02 21:21:42 +0530

    change

--- Python/ClassRoom_Progarm.py
+++ Python/ClassRoom_Progarm.py
@@ -1,32 +1,52 @@
-list=[{
-    "name": "John",
+list=[
+    {
+    "name": "Amit",
     "department": "engineering",
-    "salary": 50000
+    "salary": 75000
     },
-      {
-          "name": "Alice",
-          "department": "marketing",
-          "salary": 60000
+    {
+          "name": "Sneha",
+          "department": "engineering",
+          "salary": 82000
       },
       {
-          "name": "Bob",
+          "name": "Rahul",
           "department": "sales",
-          "salary": 55000
-      }]
+          "salary": 54000
+      },
+      {
+          "name": "Priya",
+          "department": "sales",
+          "salary": 61000
+      }
+      ]
 print("Employee Details:")
-counteng = 0
-countmar = 0
-countsal = 0
+
+
+
+
+
+
+
+dic={}
+countdep=0
+countdep1=0
+avg_salary=0
 for employee in list:
-    print("Name:", employee["name"])
-    print("Department:", employee["department"])
-    print("Salary:", employee["salary"])
     if employee["department"] == "engineering":
-        counteng += 1
+        countdep += 1
+        
+        avg_salary += employee["salary"]
+        avg_salary = avg_salary / countdep
+        dic[employee["department"]] =(countdep, avg_salary,employee["name"])
     if employee["department"] == "marketing":
-        countmar += 1
+        countdep += 1
+        avg_salary += employee["salary"]
+        avg_salary = avg_salary / countdep
+        dic[employee["department"]] =(countdep, avg_salary,employee["name"])
     if employee["department"] == "sales":
-        countsal += 1
-print("Number of employees in the engineering department:", counteng)
-print("Number of employees in the marketing department:", countmar)
-print("Number of employees in the sales department:", countsal)
+        countdep1 += 1
+        avg_salary += employee["salary"]
+        avg_salary = avg_salary / countdep1
+        dic[employee["department"]] =(countdep1, avg_salary,employee["name"])
+print(dic)        
