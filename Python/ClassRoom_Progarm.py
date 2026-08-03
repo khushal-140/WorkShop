bf766ed6cd81e3cdd7947f0a313e1709892d0bfe
Khushal Bharat Bamaniya <khushalbharat0@gmail.com>
2026-08-03 21:52:14 +0530

    change

--- Python/ClassRoom_Progarm.py
+++ Python/ClassRoom_Progarm.py
@@ -1,4 +1,4 @@
-list=[
+employees=[
     {
     "name": "Amit",
     "department": "engineering",
@@ -28,25 +28,3 @@ print("Employee Details:")
 
 
 
-dic={}
-countdep=0
-countdep1=0
-avg_salary=0
-for employee in list:
-    if employee["department"] == "engineering":
-        countdep += 1
-        
-        avg_salary += employee["salary"]
-        avg_salary = avg_salary / countdep
-        dic[employee["department"]] =(countdep, avg_salary,employee["name"])
-    if employee["department"] == "marketing":
-        countdep += 1
-        avg_salary += employee["salary"]
-        avg_salary = avg_salary / countdep
-        dic[employee["department"]] =(countdep, avg_salary,employee["name"])
-    if employee["department"] == "sales":
-        countdep1 += 1
-        avg_salary += employee["salary"]
-        avg_salary = avg_salary / countdep1
-        dic[employee["department"]] =(countdep1, avg_salary,employee["name"])
-print(dic)        
