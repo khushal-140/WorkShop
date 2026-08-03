e04bd1131f1e601f8feb8d35255deede0a17f951
Khushal Bharat Bamaniya <khushalbharat0@gmail.com>
2026-08-03 22:04:48 +0530

    change

--- Python/ClassRoom_Progarm.py
+++ Python/ClassRoom_Progarm.py
@@ -27,4 +27,34 @@ print("Employee Details:")
 
 
 
+dic={}
+count=0
 
+for employee in employees:
+    department = employee["department"]
+    salary = employee["salary"]
+    name = employee["name"]
+
+    if department not in dic:
+        dic[department] = {"count": 0,
+                           "total_salary": salary,
+                           "highest_salary": salary,
+                           "highest_salary_employee": name}
+
+    else:
+        dic[department]["count"] += 1
+        dic[department]["total_salary"] += salary
+        
+        if salary > dic[department]["highest_salary"]:
+            dic[department]["highest_salary"] = salary
+            dic[department]["highest_salary_employee"] = name
+
+# results = {}
+# for department in dic:
+#     count =dic[department]["count"]
+#     total_salary = dic[department]["total_salary"]/count
+#     highest_salary = dic[department]["highest_salary"]
+    
+#     results[department] = (count, total_salary, highest_salary)
+    
+# print(results)
